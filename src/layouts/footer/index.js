77ae1db39bb3d768a840dec3dc7b452af1808b09
user1 <vladimirbar7@gmail.com>
2019-05-01 17:08:18 +0300

    footer redesign

--- src/layouts/footer/index.js
+++ src/layouts/footer/index.js
@@ -3,25 +3,33 @@ import Link from '../../components/common/link';
 import './style.styl';
 
 const Footer = () => (
-  <footer className="footer">
-    <div className="container">
-      <div className="footer__socials">
-        <Link href="https://facebook.com" blank>Facebook</Link>
-        <Link href="https://instagram.com" blank>Instagram</Link>
-      </div>
-      <div className="footer__contactInfo">
-        <a href="tel:380937625988"><strong>+380937625988</strong></a>
-        <a href="mailto:trpadvntr@gmail.com">trpadvntr@gmail.com</a>
+  <footer className="footer container">
+    <div className="footer__contactInfo">
+      <div>
+        <i className="fas fa-phone" />
+        <Link href="tel:380937625988">+38(093)-76-25-988</Link>
       </div>
       <div>
-        <ul className="footer__links">
-          <li><Link href="/articles">Статьи</Link></li>
-          <li><Link href="/reviews">Отзывы</Link></li>
-          <li><Link href="/private-tours">Индивидуальные туры</Link></li>
-          <li><Link href="/photos">Фотоотчеты</Link></li>
-          <li><Link href="/contacts">Контакты</Link></li>
-        </ul>
+        <i className="fas fa-envelope" />
+        <Link href="mailto:trpadvntr@gmail.com">trpadvntr@gmail.com</Link>
       </div>
+    </div>
+    <div>
+      <ul className="footer__links">
+        <li><Link href="/private-tours">Индивидуальные туры</Link></li>
+        <li><Link href="/photos">Фотоотчеты</Link></li>
+        <li><Link href="/contacts">Контакты</Link></li>
+        <li><Link href="/reviews">Отзывы</Link></li>
+        <li><Link href="/articles">Статьи</Link></li>
+      </ul>
+    </div>
+    <div className="footer__socials">
+      <Link href="https://facebook.com" blank>
+        <i className="fab fa-facebook-f" />
+      </Link>
+      <Link href="https://instagram.com" blank>
+        <i className="fab fa-instagram" />
+      </Link>
     </div>
   </footer>
 );
